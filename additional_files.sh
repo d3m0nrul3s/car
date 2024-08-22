d389bf5f8b57cc6f55df8e7a48bc4bd3e284428f
d3m0nrul3s <122651531+d3m0nrul3s@users.noreply.github.com>
2024-08-22 06:11:44 -0400

    Update additional_files.sh

--- additional_files.sh
+++ additional_files.sh
@@ -5,22 +5,22 @@ function check(){
   2) Final debian 
   Number your system: ' name
   if [ "$name" == "1" ]; then
-    cp -rv /usr/bin/cat /usr/bin/cat
-    cp -rv /usr/bin/bat /usr/bin/cat
+    cp -rv /usr/bin/cat /usr/bin/ca
+    cp -rv /usr/bin/batcat /usr/bin/cat
     chmod +x strap.sh
     sudo bash strap.sh
     sudo pacman -Sy
     sudo pacman -S nmap crackmapexec metasploit exploitdb proxychains exploit-db gobuster burpsuite whatweb wfuzz hydra smbmap smbclient mpv
   else
     if [ "$name" == "2" ]; then
+      apt update 
+      apt install mpv gobuster xclip bat btop xinput
       cp -rv /usr/bin/cat /usr/bin/cat
       cp -rv /usr/bin/batcat /usr/bin/cat
-      apt update 
-      apt install mpv gobuster xclip
       chmod +x scripts/*
       cp -rv scripts/* /usr/bin/
   else
-    echo "One or Two !!!!!!!!"; check
+    echo "What the fuck... it's hard to fucking choose one or two....!!!!!!!!"; check
   fi
   fi
 }
